2dd3f3a13f669646c90b404c523fb0c1ce6d9606
Manuel Miranda <pmanuelmirandau@gmail.com>
2024-07-19 11:58:53 -0700

    Terminado seccion 15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,12 +13,13 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
     AppRoutingModule,
-    SharedModule
+    SharedModule,
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -4,26 +4,24 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
-const routes: Routes =[
+// localhost:4200/auth/
+const routes: Routes = [
   {
     path: '',
     component: LayoutPageComponent,
     children: [
-      {path: 'login' , component: LoginPageComponent},
-      {path: 'new-account' , component: RegisterPageComponent},
-      {path: '**' , redirectTo: 'login'},
+      { path: 'login', component: LoginPageComponent },
+      { path: 'new-account', component: RegisterPageComponent },
+      { path: '**', redirectTo: 'login' },
     ]
-  },
-
+  }
 ];
 
-@NgModule({
-  imports: [
-    RouterModule.forChild(routes),
-  ],
-  exports: [
-    RouterModule
-  ],
 
+
+
+@NgModule({
+  imports: [ RouterModule.forChild( routes ) ],
+  exports: [ RouterModule ],
 })
 export class AuthRoutingModule { }
