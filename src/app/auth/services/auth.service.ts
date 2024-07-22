dab0d1f6955135a59b7759a88ce0b3c846aa392d
Manuel Miranda <pmanuelmirandau@gmail.com>
2024-07-22 09:40:44 -0700

    Seccion 16

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environments } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, tap, of, map, catchError } from 'rxjs';
+
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { environments } from '../../../environments/environment';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
@@ -12,20 +14,41 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  getcurrentUser(): User|undefined {
-    if( !this.user) return undefined;
-    return structuredClone( this.user)
+  get currentUser():User|undefined {
+    if ( !this.user ) return undefined;
+    return structuredClone( this.user );
   }
 
-  login(email: string, password: string): Observable<User>{
-
-    return this.http.get<User>(`${this.baseUrl}/users/1`)
-    .pipe(
-      tap( user => this.user = user),
-      tap( user => localStorage.setItem('token', user.id.toString())),
-    );
+  login( email: string, password: string ):Observable<User> {
+    // http.post('login',{ email, password });
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        tap( user => localStorage.setItem('token', 'aASDgjhasda.asdasd.aadsf123k' )),
+      );
   }
+
+  checkAuthentication(): Observable<boolean> {
+
+    if ( !localStorage.getItem('token') ) return of(false);
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        map( user => !!user ),
+        catchError( err => of(false) )
+      );
+
+  }
+
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
+  }
+
+
 
 }
-
-
